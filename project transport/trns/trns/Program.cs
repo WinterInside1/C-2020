bd9e09dc605173b245ade900878ab5c7dc7dc0ef
DimaPriest <dima6789@icloud.com>
2020-02-24 06:20:35 -0800

    all prog2

--- project transport/trns/trns/Program.cs
+++ project transport/trns/trns/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Bmw;
 using Mersedes;
 
 namespace @class
@@ -13,45 +10,50 @@ namespace @class
         public int price;
         protected string comfort;
         protected double speed;
-       
+
         public int year { get; set; }
-        protected string color = "нет";           
+        protected string color = "нет";
+
         public int Number
         {
-            get {return TransportNumber(); }
-            set {; }
+            get { return TransportNumber(); }
+            set { ; }
         }
-        protected string str0 ;
+
+        protected string str0;
         protected bool res = true;
+
         public string TrColor
         {
-            get {return color; }
-            set {color = value ; }
+            get { return color; }
+            set { color = value; }
         }
 
-        
+
         public string Name
         {
-            get { return name ; }
-            set { name = value;  }
+            get { return name; }
+            set { name = value; }
         }
 
-        public Transport() {
+        public Transport()
+        {
             name = "Transport";
             price = 0;
             comfort = "high";
             speed = 0.0;
             year = 2000;
-            
+
         }
 
-        public Transport(string _name, int _price, string _comfort, double _speed, int _year) {
+        public Transport(string _name, int _price, string _comfort, double _speed, int _year)
+        {
             name = _name;
             price = _price;
             comfort = _comfort;
             speed = _speed;
             year = _year;
-            
+
         }
 
         public virtual void show()
@@ -85,6 +87,7 @@ namespace @class
                     res = true;
                 }
             }
+
             price = Convert.ToInt32(str0);
             Console.WriteLine("Comfort: ");
             comfort = Console.ReadLine();
@@ -105,6 +108,7 @@ namespace @class
                     res = true;
                 }
             }
+
             speed = Convert.ToDouble(str0);
             Console.WriteLine("Year: ");
             str0 = Console.ReadLine();
@@ -123,9 +127,10 @@ namespace @class
                     res = true;
                 }
             }
+
             year = Convert.ToInt32(str0);
-            Console.WriteLine();            
-        }        
+            Console.WriteLine();
+        }
 
         public void GetColor(string cl)
         {
@@ -136,7 +141,7 @@ namespace @class
         {
             int numb;
             Random rand = new Random();
-            numb = rand.Next(1000,10000);
+            numb = rand.Next(1000, 10000);
             return numb;
         }
 
@@ -145,167 +150,21 @@ namespace @class
             Console.WriteLine("Имя: Транспорт\n Содержит базовые характеристики видов транспортных средств");
         }
 
-        
-                    
+
+
     }
 
 
-namespace trns
-{
-    class Program
+    namespace trns
     {
-        static int cash;
-        static void menu(Transport[] mas)
+        class Program
         {
-            int i = 0, j = 0;
-            Console.WriteLine("У вас есть $" + Convert.ToString(cash));
-            Console.WriteLine("1 - Информация о классе");
-            Console.WriteLine("2 - Ввод новых данных");
-            Console.WriteLine("3 - Показать информацию об объектах");
-            Console.WriteLine("4 - Выбрать цвет для ТС");
-            Console.WriteLine("5 - Показать цвет ТС");
-            Console.WriteLine("6 - Показать номер");
-            Console.WriteLine("7 - Перейти к выбору автомобилей");
-            Console.WriteLine("0 - Выход");
+            static int cash;
 
-            mas[0] = new Transport();
-            string str;
-            bool yes = true;
-            while (yes == true)
+            static void menu(Transport[] mas)
             {
-
-                str = Console.ReadLine();
-                char symb;
-                bool res = true;
-                while (res == true)
-                {
-                    res = false;
-
-                    try
-                    {
-                        symb = Convert.ToChar(str);
-                    }
-                    catch (System.FormatException)
-                    {
-                        Console.WriteLine("Неизвестный символ.Повторите попытку");
-                        str = Console.ReadLine();
-                        res = true;
-                    }
-
-                }
-                symb = Convert.ToChar(str);
-                switch (symb)
-                {
-                    case '1': Transport.info(); break;
-                    case '2':
-                        if (i > 0)
-                        { Array.Resize(ref mas, i + 1);
-                            mas[i] = new Transport();
-                            mas[i].enter();
-                            i++;
-                        }
-                        else
-                        {
-                            mas[i].enter();
-                            i++;
-                        }
-                        break;
-                    case '3':
-                        for (j = 0; j < mas.Length; j++)
-                        {
-                            mas[j].show();
-                        }
-                        break;
-                    case '4':
-                        Console.WriteLine("Введите название ТС");
-                        string ObjName = Console.ReadLine();
-                        for (j = 0; j < mas.Length; j++)
-                        {
-                            if (mas[j].Name == ObjName)
-                            {
-                                Console.WriteLine();
-                                var col = Console.ReadLine();
-                                mas[j].GetColor(col);
-                            }
-                            else
-                                if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
-                        }
-                        break;
-                    case '5':
-                        Console.WriteLine("Введите название ТС");
-                        string ObjName1 = Console.ReadLine();
-                        for (j = 0; j < mas.Length; j++)
-                        {
-                            if (mas[j].Name == ObjName1)
-                            {
-                                Console.WriteLine(mas[j].TrColor);
-                            }
-                            else
-                                if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
-                        }
-                        break;
-                    case '6':
-                        Console.WriteLine("Введите название ТС");
-                        string ObjName2 = Console.ReadLine();
-                        for (j = 0; j < mas.Length; j++)
-                        {
-                            if (mas[j].Name == ObjName2)
-                            {
-                                Console.WriteLine(mas[j].Number);
-                            }
-                            else
-                                if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
-                        }
-                        break;
-
-                    case '7':
-                        bool res1 = true;
-                        while (res1 == true)
-                        {
-                            Console.WriteLine("1 - Mersedes");
-                            Console.WriteLine("2 - BMW");
-                            Console.WriteLine("3 - Cancel");
-                            Console.WriteLine("0 - exit");
-                            char s1;
-                            string str3 = Console.ReadLine();
-                            while (res1 == true)
-                            {
-                                res1 = false;
-                                try { s1 = Convert.ToChar(str3); }
-                                catch (System.FormatException)
-                                {
-                                    Console.WriteLine("Введено неверное значение. Повторите попытку");
-                                    str3 = Console.ReadLine();
-                                    res1 = true;
-                                }
-                            }
-                            res1 = true;
-                            s1 = Convert.ToChar(str3);
-                            switch (s1)
-                            {
-                                case '1':
-                                    Mers[] arr1 = new Mers[1];
-                                    Menu.MenuMers(arr1, cash);
-                                    break;
-                                case '2':
-                                    BMW[] arr2 = new BMW[1];
-                                    Menu.BMWMenu(arr2, cash);
-                                    break;
-                                case '3':
-                                    res1 = false;
-                                    break;
-                                case '0': return;
-                                default: Console.WriteLine("Неизвестный символ"); break;
-                            }
-                            
-                        }break;
-                    case '0':
-                        yes = false;
-                        return;
-
-                    default: Console.WriteLine("Неизвестный символ"); break;
-                    
-                }
+                int i = 0, j = 0;
+                Console.WriteLine("У вас есть $" + Convert.ToString(cash));
                 Console.WriteLine("1 - Информация о классе");
                 Console.WriteLine("2 - Ввод новых данных");
                 Console.WriteLine("3 - Показать информацию об объектах");
@@ -313,21 +172,186 @@ namespace trns
                 Console.WriteLine("5 - Показать цвет ТС");
                 Console.WriteLine("6 - Показать номер");
                 Console.WriteLine("7 - Перейти к выбору автомобилей");
-
                 Console.WriteLine("0 - Выход");
-            }
-        }
 
-        static void Main(string[] args)
-        {
-            Transport[] arr = new Transport[1];
-            cash = 10000;
-            arr[0] = new Transport();
-            menu(arr);
-            Mers mers = new Mers();
-            BMW bmw = new BMW();
-            Console.WriteLine("\n"+ Convert.ToString(mers.CompareTo(bmw)));
-            
+                mas[0] = new Transport();
+                string str;
+                bool yes = true;
+                while (yes == true)
+                {
+
+                    str = Console.ReadLine();
+                    char symb;
+                    bool res = true;
+                    while (res == true)
+                    {
+                        res = false;
+
+                        try
+                        {
+                            symb = Convert.ToChar(str);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Неизвестный символ.Повторите попытку");
+                            str = Console.ReadLine();
+                            res = true;
+                        }
+
+                    }
+
+                    symb = Convert.ToChar(str);
+                    switch (symb)
+                    {
+                        case '1':
+                            Transport.info();
+                            break;
+                        case '2':
+                            if (i > 0)
+                            {
+                                Array.Resize(ref mas, i + 1);
+                                mas[i] = new Transport();
+                                mas[i].enter();
+                                i++;
+                            }
+                            else
+                            {
+                                mas[i].enter();
+                                i++;
+                            }
+
+                            break;
+                        case '3':
+                            for (j = 0; j < mas.Length; j++)
+                            {
+                                mas[j].show();
+                            }
+
+                            break;
+                        case '4':
+                            Console.WriteLine("Введите название ТС");
+                            string ObjName = Console.ReadLine();
+                            for (j = 0; j < mas.Length; j++)
+                            {
+                                if (mas[j].Name == ObjName)
+                                {
+                                    Console.WriteLine();
+                                    var col = Console.ReadLine();
+                                    mas[j].GetColor(col);
+                                }
+                                else if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
+                            }
+
+                            break;
+                        case '5':
+                            Console.WriteLine("Введите название ТС");
+                            string ObjName1 = Console.ReadLine();
+                            for (j = 0; j < mas.Length; j++)
+                            {
+                                if (mas[j].Name == ObjName1)
+                                {
+                                    Console.WriteLine(mas[j].TrColor);
+                                }
+                                else if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
+                            }
+
+                            break;
+                        case '6':
+                            Console.WriteLine("Введите название ТС");
+                            string ObjName2 = Console.ReadLine();
+                            for (j = 0; j < mas.Length; j++)
+                            {
+                                if (mas[j].Name == ObjName2)
+                                {
+                                    Console.WriteLine(mas[j].Number);
+                                }
+                                else if (j + 1 > i) Console.WriteLine("Такого ТС не существует");
+                            }
+
+                            break;
+
+                        case '7':
+                            bool res1 = true;
+                            while (res1 == true)
+                            {
+                                Console.WriteLine("1 - Mersedes");
+                                Console.WriteLine("2 - BMW");
+                                Console.WriteLine("3 - Cancel");
+                                Console.WriteLine("0 - exit");
+                                char s1;
+                                string str3 = Console.ReadLine();
+                                while (res1 == true)
+                                {
+                                    res1 = false;
+                                    try
+                                    {
+                                        s1 = Convert.ToChar(str3);
+                                    }
+                                    catch (System.FormatException)
+                                    {
+                                        Console.WriteLine("Введено неверное значение. Повторите попытку");
+                                        str3 = Console.ReadLine();
+                                        res1 = true;
+                                    }
+                                }
+
+                                res1 = true;
+                                s1 = Convert.ToChar(str3);
+                                switch (s1)
+                                {
+                                    case '1':
+                                        Mers[] arr1 = new Mers[1];
+                                        Menu.MenuMers(arr1, cash);
+                                        break;
+                                    case '2':
+                                        BMW[] arr2 = new BMW[1];
+                                        Menu.BMWMenu(arr2, cash);
+                                        break;
+                                    case '3':
+                                        res1 = false;
+                                        break;
+                                    case '0': return;
+                                    default:
+                                        Console.WriteLine("Неизвестный символ");
+                                        break;
+                                }
+
+                            }
+
+                            break;
+                        case '0':
+                            yes = false;
+                            return;
+
+                        default:
+                            Console.WriteLine("Неизвестный символ");
+                            break;
+
+                    }
+
+                    Console.WriteLine("1 - Информация о классе");
+                    Console.WriteLine("2 - Ввод новых данных");
+                    Console.WriteLine("3 - Показать информацию об объектах");
+                    Console.WriteLine("4 - Выбрать цвет для ТС");
+                    Console.WriteLine("5 - Показать цвет ТС");
+                    Console.WriteLine("6 - Показать номер");
+                    Console.WriteLine("7 - Перейти к выбору автомобилей");
+
+                    Console.WriteLine("0 - Выход");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Transport[] arr = new Transport[1];
+                cash = 10000;
+                arr[0] = new Transport();
+                menu(arr);
+                Mers mers = new Mers();
+                BMW bmw = new BMW();
+                Console.WriteLine("\n" + Convert.ToString(mers.CompareTo(bmw)));
+
+            }
         }
     }
 }
